8b7aef553d0f7bae6ccf3a48c1b30e8e9ed4716c
DeepikaJeyaganesh <kdeepika101@gmail.com>
2024-03-06 17:19:28 +0530

    added cui application

---
+++ CUIApplication/LibraryManagementService/ResponseObject.cs
@@ -0,0 +1,63 @@
+﻿using LibraryManagementVo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementService
+{
+    public class ResponseObject
+    {
+        private string successMessage;
+
+        private List<BookVo> bookVoList;
+
+        public List<BookVo> GetBookVoList()
+        {
+            return bookVoList;
+        }
+
+        public void SetBookVoList(List<BookVo> value)
+        {
+            bookVoList = value;
+        }
+
+        public string GetSuccessMessage()
+        {
+            return successMessage;
+        }
+
+        public void SetSuccessMessage(string value)
+        {
+            successMessage = value;
+        }
+
+        private BookVo vo;//fetch by id
+
+
+        public BookVo GetVo()
+        {
+            return vo;
+        }
+
+        public void SetVo(BookVo value)
+        {
+            vo = value;
+        }
+
+        private string failureMessage;
+
+
+
+        public string GetFailureMessage()
+        {
+            return failureMessage;
+        }
+
+        public void SetFailureMessage(string value)
+        {
+            failureMessage = value;
+        }
+    }
+}
