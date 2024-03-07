256690441312afeddfd4a32c9a47b9d8a6d4f22f
360Ripples <360Ripples@gmail.com>
2024-03-07 11:24:18 +0530

    added source formatting

--- CUIApplication/LibraryManagementBo/LibraryBo/BookBo.cs
+++ CUIApplication/LibraryManagementBo/LibraryBo/BookBo.cs
@@ -8,23 +8,15 @@ using System.Data;
 namespace LibraryManagementBo.LibraryBo
 {
 
-
     public class BookBo
     {
-
-
-
         IBookDao bookDao = new BookDao();
         BookManagementLogger logger = new BookManagementLogger();
-
-
 
         public bool InsertBook(BookVo vo)
         {
             bool flag = false;
             bool b = false;
-
-
             try
             {
                 Log.Information("Adding a Book {Vo}", vo);
@@ -43,13 +35,9 @@ namespace LibraryManagementBo.LibraryBo
                     {
                         Log.Debug("ISBN RULE 1:ISBN invalid for the given id {Id}", vo.id);
                         return b;
-
                     }
-
                     sum += digit * (10 - i);
                 }
-
-
                 char last = vo.isbn[9];
                 if (last != 'X' && (last < '0'
                              || last > '9'))
@@ -57,11 +45,7 @@ namespace LibraryManagementBo.LibraryBo
                     Log.Debug("ISBN RULE 2:ISBN invalid for the given id {Id}", vo.id);
                     return b;
                 }
-
-
                 sum += last == 'X' ? 10 : last - '0';
-
-
                 b = sum % 11 == 0;
                 if (b)
                 {
@@ -71,9 +55,7 @@ namespace LibraryManagementBo.LibraryBo
                 {
                     throw new BookManagementException("ISBN Number Not Valid.Plz give the Valid one and proceed with Adding Book Details..");
                 }
-
                 flag = bookDao.AddBook(vo);//true
-
                 Log.Information("Adding Book {BookResponce}response...", flag);
             }
             catch (BookManagementException e)
@@ -83,25 +65,18 @@ namespace LibraryManagementBo.LibraryBo
             }
 
             return flag;//true
-
         }
         public BookVo GetBook(int id)
         {
             BookVo vo = new BookVo();
             vo = bookDao.FetchBook(id);
             return vo;
-
-
         }
         public List<BookVo> GetBooks()
         {
-
             List<BookVo> list = new List<BookVo>();
             list = bookDao.FetchBooks();
             return list;
-
         }
-
-
     }
 }
